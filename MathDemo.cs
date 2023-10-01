2c5f8666127d924e730ac0e67750eb760bcdc21c
transitionmaster-training <transitionmaster.training@gmail.com>
2023-10-01 09:57:49 +0530

    DotNet Hello Applicatin-Transition

---
+++ MathDemo.cs
@@ -0,0 +1,7 @@
+﻿namespace HelloWorldConsoleApp
+{
+    public class MathDemo
+    {
+        public int Add(int num1, int num2) { return num1 + num2; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloWorldConsoleApp
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            MathDemo mathDemo = new MathDemo();
+            Console.WriteLine(mathDemo.Add(2, 4));
+        }
+    }
+}
